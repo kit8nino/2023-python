ff6afce10f6cc6f18b66ffa0740ab193a6a3ce85
PolinaShulpina <127147539+PolinaShulpina@users.noreply.github.com>
2023-03-28 17:07:33 +0300

    [1]

--- ИС34/Шульпина Полина/1 лаб.py
+++ ИС34/Шульпина Полина/1 лаб.py
@@ -18,7 +18,7 @@ subjects = {
 'астрономия':5,
 'икт':5,
 }
-relatives=['Света','Олег','Артем','Валя','Паша','Коля','Люся','Маша','Женя']
+relatives=['Света','Олег','Артем','Валя','Валя','Паша','Коля','Люся','Маша','Женя']
 cat=('Asya')
 n=len(subjects.values())
 mean_mark=0
@@ -48,23 +48,72 @@ print('3.общая длина всех названий предметов:',le
 print('4.уникальные буквы в названиях предметов',unletters)
 bin_result = ''.join(format(x,'08b') for x in bytearray(cat,'utf-8'))
 print('5.имя кивы в бинарном виде:',bin_result)
-print('6.отсортированный в обратном порядке список родственников:',relatives[::-1])
+not_repeat.sort()
+print('6.отсортированный в обратном алфавитном порядке список родственников:',not_repeat[::-1])
 from datetime import date
 birth = date(2004,10,17)
 today=date.today()
 difference_days=today-birth
-print('7.Количество дней от вашей даты рождения до текущей даты:',difference_days)
+
+print('\n7.Количество дней от вашей даты рождения до текущей даты:',difference_days)
 rulers=['Теноч','Acamapichtli','Уицилиуитль','Чимальпопока',
     'Сихуитль Темок','Ицкоатль','Монтесума I','Атотоцтли','Аксайакатль','Тизок',
     'Ауицотль','Моктесума II','Cuitláhuac','Cuauhtémoc','Тлакотзин','Мотельчюхцин',
     'Сочиквентзин','Хуаницин','Теуэцквитицин','Чечетцин','Cipac']
+FIFO=[]
+stop='стоп'
+while True:
+    queue=input('8.введите значение (слово для остановки - стоп):')
+    if queue==stop:
+        for i in range(len(FIFO)):
+            print(FIFO.pop(),end=' ')
+        break
+    FIFO.append(queue)
 
 
 number=(17+17**2+2004)%21+1
-print('введите индекс от 0 до 9')
-i=int(input())
-relatives[i]=rulers[number]
-print('9.Итог:',relatives[i])
+print('введите индекс от 1 до 10')
+i=int(input())-1
+new_relatives=[]
+for elem in not_repeat:
+    new_relatives.append(elem)
+new_relatives[i]=rulers[number]
+print('9.Итог:',new_relatives)
+
+text={}
+for x in range(len(not_repeat)-1):
+    text[not_repeat[x]]=not_repeat[x+1]
+print('10.Связный список как словарь:',text)
+variant=len(data) * len (relatives) % 4
+print(variant,'вариант')
+from math import sqrt
+def getSum(el):
+    summ = 0
+    for i in range(1, int(sqrt(el)) + 1):
+        if el % i == 0:
+            if el // i == i:
+                summ += i
+            else:
+                summ += i
+                summ += el // i
+    return summ - el
+def Aliquot(el):
+    print(el, end=" ")
+    s = set()
+    s.add(el)
+    nextt = 0
+    while el > 0:
+        el = getSum(n)
+
+        if el in s:
+            print("Repeats with", el)
+            break
+        print(el, end=" ")
+        s.add(el)
+
+k=int(input('число для аликвотной последовательности:'))
+Aliquot(k)
+
 
 
 
