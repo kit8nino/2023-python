53f89b50541703335c86ba443f6edbc399f951c2
kit8nino <freezer.nik@gmail.com>
2023-05-23 09:29:23 +0300

    [4]

--- _4/mob.py
+++ _4/mob.py
@@ -7,8 +7,9 @@ class Mob(turtle.Turtle):
     def __init__(self, coord=(0, 0), name='Frog', img='frog.gif',
                  speed=15):
         super().__init__()
-        self.register_shape(name='frog', shape='frog.gif')
-        self.shape('frog')
+        # scr = turtle.Screen()
+        # scr.register_shape(shape='./frog.gif', name='frog')
+        self.shape(name='turtle')
         self.penup()
         self.coord = list(coord)
         self.goto(coord)
@@ -29,3 +30,6 @@ class Mob(turtle.Turtle):
             self.coord[0] += self.speed
         self.move()
         return self.coord
+
+    def set_coord(self, coord):
+        self.coord = list(coord)
